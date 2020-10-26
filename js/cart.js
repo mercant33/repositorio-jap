4d2e04b2f1243599321027bc61e0f67f2e32be9a
mercant33 <68981439+mercant33@users.noreply.github.com>
2020-10-25 21:09:20 -0300

    Septimo Commit/Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-let subTotel = 0;
+
 let totalCost = 0;
 let productCost = 0;
 let MONEY_SYMBOL = "$";
@@ -25,6 +25,7 @@ function showCart(articles) {
                 <input type="number" class="form-control cantidades" id="cantidad`+ i + `" placeholder="" value="` + articles[i].count + `" min="1">
             </td>
             <td id="caca" scope="col"><span id="subtotal`+ i + `" class="subTotales" style="font-weight:bold;">`+ articles[i].currency +` `+ subTotel +`</span>  </td>
+            <td class="text-center"><button onclick="deleteFile(`+ i +`)" class="btn btn-danger">Eliminar</button></td>
         </tr>
         `
         document.getElementById('carritoInfo').innerHTML = htmlContentToAppend;
@@ -49,35 +50,42 @@ function showCart(articles) {
     }}
 }
 
+// E6 - Eliminar filas de productos
+function deleteFile(id) {
+document.getElementById('tr' +id).remove();
+}
+
 function updateTotalCosts() {
-    let sub0 = document.getElementById('subtotal0').outerText;
-    let sub1 = document.getElementById('subtotal1').outerText;
-    arrey0 = sub0.split(" ");
-    arrey1 = sub1.split(" ");
-    re0 = parseInt(arrey0[1]);
-    re1 = parseInt(arrey1[1] * 40);
-    subTotel = re0 + re1;
+    // E6 - Modificado para que se realice el degloce de costos genérico
+    let subTotel = 0;
+    var subtotals = document.getElementsByClassName('subTotales');
+    for (let d = 0; d < subtotals.length; d++) {
+        let spen = subtotals[d].outerText;
+        arrey = spen.split(" ");
+        price = parseInt(arrey[1]);
+        if (arrey[0] === "USD") {
+            price = price * 40;
+        }
+        subTotel = subTotel + price;
+        console.log(price);
+    }
     
-    let productCost = subTotel;
     let unitProductCost = document.getElementById('productCostText');
-    let unitCostToShow = MONEY_SYMBOL + productCost;
-    unitProductCost.innerHTML = unitCostToShow;
+    unitProductCost.innerHTML = PESO_SYMBOL + subTotel;
 
     let comissionCostHTML = document.getElementById('comissionText');
-    let comissionToShow = Math.round((comissionPercentage * 100)) + PERCENTAGE_SYMBOL;
-    comissionCostHTML.innerHTML = comissionToShow;
+    comissionCostHTML.innerHTML = PESO_SYMBOL + subTotel * comissionPercentage;
 
     let totalCostHTML = document.getElementById('totalCostText');
-    let totalCost = (Math.round(productCost * comissionPercentage) + productCost);
-    totalCostHTML.innerHTML = totalCost;
+    let totalCost = (Math.round(subTotel * comissionPercentage) + subTotel);
+    totalCostHTML.innerHTML = PESO_SYMBOL + totalCost;
 
     var inputsT = document.getElementsByClassName('cantidades');
     for (var b=0; b < inputsT.length; b++){
     inputsT[b].addEventListener("change", function () {
-        productCost = this.value;
+        subTotel = this.value;
         updateTotalCosts();
       })};
-
 
 
     // E5 - Eventos correspondientes a cambios en el tipo de envío (muestra porcentaje)
@@ -97,20 +105,75 @@ function updateTotalCosts() {
     });
 }
 
-document.getElementById('buyIt').addEventListener('click', function() {
-  alert("grazie per l'acquisto");
-});
+
+// E6 - Validación método de pago
+var payForm = document.getElementsByName('pagoMetodo')[0];
+formElements = payForm.elements;
+creditChoice = document.getElementById('');
+transferChoice = document.getElementById('');
+boton = document.getElementById('metodoSeleccionado');
+
+var metodoTarjeta = function(e) {
+    if( formElements.bancoTarjeta.value == "" || formElements.numeroTarjeta.value < 12 || formElements.nombreApellidoTarjeta.value < 8 || formElements.vencimientoTarjeta.value < 5 || formElements.seguridadTarjeta.value < 3) {
+        alert("Complete los campos de su tarjeta bancaria");
+        e.preventDefault();
+    } else {
+        alert("Su pago se realizará mediante tarjeta bancaria");
+        document.getElementById('metodoElecto').innerHTML = "Método Tarjeta Bancaria";
+
+    }
+}
+
+var metodoBancario = function(e) {
+    if ( formElements.nombreTransferencia.value < 8 || formElements.bancoTransferencia.value == "" || formElements.cuentaTransferencia.value < 5) {
+        alert("Complete los campos para método bancario");
+        e.preventDefault();
+    } else {
+        alert("Su pago se realizará mediante transferencia bancaria");
+        document.getElementById('metodoElecto').innerHTML = "Método Bancario";
+    }
+}
+
+var inputElected = function(e) {
+    if ( payForm.metodoDePago[0].checked == true ) {
+        metodoTarjeta(e);
+
+    } else {
+        metodoBancario(e);
+    }
+}
+boton.addEventListener('click', inputElected);
+
+// E6 - Validación de formulario principal
+document.getElementById('finalizarCompra').addEventListener('click', finishBuy);
+  function finishBuy(e) {
+      var street = document.getElementById('calle').value;
+      var number = document.getElementById('numeroCalle').value;
+      var corner = document.getElementById('esquina').value;
+      var metodo = document.getElementById('metodoElecto').value;
+
+      if ( street.length >= 2 && number !== "" && corner.length >= 2 && metodo !== "") {
+        alert(buyOk);
+      } else {
+          alert('quedan campos por completar');
+          e.preventDefault();
+          return 0;
+      }
+  }
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(DESAFIATE).then(function (resultObj) {
         if (resultObj.status === "ok") {
             cartInfo = resultObj.data.articles;
             showCart(cartInfo);
             updateTotalCosts();
-
         }
     });
+    getJSONData(CART_BUY_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            buyOk = resultObj.data.msg;
+        }});
 });
